eabce2ebc9e296bbcf722238a96a36475aed998d
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-08-18 17:29:37 +0200

    feat: udate script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# this script is used to update the docker image
+ 
+LATEST_VERSION=$(curl https://mikrotik.com/download/archive -s | grep Release -m 1 | grep -oP '(?<=<b>Release )\d+\.\d+')
+
+echo "Latest version is $LATEST_VERSION"
+sed -r "s/(ROUTEROS_VERSON=\")(.*)(\")/\1$LATEST_VERSION\3/g" -i Dockerfile
+
+exit
+
+docker build -t ghibranalj/docker-routeros:$LATEST_VERSION .
+
+docker push ghibranalj/docker-routeros:$LATEST_VERSION
