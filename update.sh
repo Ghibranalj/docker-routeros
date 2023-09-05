3716be83748056b266d7fc25f1299fa8687e5acb
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-09-05 21:13:13 +0200

    feat: github actions and auto update all version

--- update.sh
+++ update.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 # this script is used to update the docker image
  
-LATEST_VERSION=$(curl https://mikrotik.com/download/archive -s | grep Release -m 1 | grep -oP '(?<=<b>Release )\d+\.\d+')
+VERSIONs=$(curl https://mikrotik.com/download/archive -s | awk -F ' ' '/Release/{gsub(/<\/b>/, "", $3);print $3}')
 
-echo "Latest version is $LATEST_VERSION"
+# loop through the versions
+for VERSION in VERSIONs; do
+    docker build -t ghibranalj/docker-routeros:$VERSION . --build-arg "ROUTEROS_VERSION=$VERSION"
+    docker push ghibranalj/docker-routeros:$VERSION
+done
 
-docker build -t ghibranalj/docker-routeros:$LATEST_VERSION . --build-arg "ROUTEROS_VERSION=$LATEST_VERSION"
 
-docker push ghibranalj/docker-routeros:$LATEST_VERSION
+# Get latest Version
+LATEST=$(echo $VERSIONs | head -n 1)
+# rename tag to latest
+echo Renaming Version $LATEST to :latest
+docker tag ghibranalj/docker-routeros:$LATEST ghibranalj/docker-routeros:latest
 
-docker tag ghibranalj/docker-routeros:$LATEST_VERSION ghibranalj/docker-routeros:latest
-
-docker push ghibranalj/docker-routeros:latest
