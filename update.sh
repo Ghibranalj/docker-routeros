e41387c85f652e6fc8585b40f2e64579d2632cdc
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-09-05 21:29:28 +0200

    fix: broken script

--- update.sh
+++ update.sh
@@ -4,15 +4,14 @@
 VERSIONs=$(curl https://mikrotik.com/download/archive -s | awk -F ' ' '/Release/{gsub(/<\/b>/, "", $3);print $3}')
 
 # loop through the versions
-for VERSION in VERSIONs; do
+for VERSION in $VERSIONs; do
     docker build -t ghibranalj/docker-routeros:$VERSION . --build-arg "ROUTEROS_VERSION=$VERSION"
     docker push ghibranalj/docker-routeros:$VERSION
 done
 
 # Get latest Version
-LATEST=$(echo $VERSIONs | head -n 1)
+LATEST=$(echo $VERSIONs | awk '{print $1}')
 # rename tag to latest
 echo Renaming Version $LATEST to :latest
 docker tag ghibranalj/docker-routeros:$LATEST ghibranalj/docker-routeros:latest
-
 docker push ghibranalj/docker-routeros:latest
