a87fb78f970b9d86db1e7dae30937fa6fcb43aba
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-09-05 21:45:57 +0200

    fix: versionings

--- update.sh
+++ update.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env sh
 # this script is used to update the docker image
  
-VERSIONs=$(curl https://mikrotik.com/download/archive -s | awk -F ' ' '/Release/{gsub(/<\/b>/, "", $3);print $3}')
-
+VERSIONs=$(curl https://mikrotik.com/download/archive -s | awk -F ' ' '/Release/{gsub(/<\/b>/, "", $3);print $3}' | tac)
+LATEST=$(curl https://mikrotik.com/download/archive -s | awk -F ' ' '/Release/{gsub(/<\/b>/, "", $3);print $3}' | head -n 1)
 # loop through the versions
 for VERSION in $VERSIONs; do
+    cat <<EOF
+=============
+Building Version: $VERSION
+=============
+EOF
     docker build -t ghibranalj/docker-routeros:$VERSION . --build-arg "ROUTEROS_VERSION=$VERSION"
     docker push ghibranalj/docker-routeros:$VERSION
 done
 
-# Get latest Version
-LATEST=$(echo $VERSIONs | awk '{print $1}')
-# rename tag to latest
-echo Renaming Version $LATEST to :latest
+    cat <<EOF
+=============
+Latest Version: $LATEST
+=============
+EOF
 docker tag ghibranalj/docker-routeros:$LATEST ghibranalj/docker-routeros:latest
 docker push ghibranalj/docker-routeros:latest
